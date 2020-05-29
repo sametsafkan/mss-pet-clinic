56e6bd4dd70180770074b2d587102c1bcea3fb7e
sametsafkan <D6cf53ec36>
2020-05-29 20:01:06 +0300

    initial commit.

---
+++ src/main/java/com/sametsafkan/msspetclinic/MssPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.sametsafkan.msspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MssPetClinicApplication.class, args);
+    }
+
+}
